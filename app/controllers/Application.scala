84fa787b60117a41beb180dde5c2aca0a168fd21
Stan <lucidhope@gmail.com>
2013-11-23 07:14:46 -0800

    Added logic to calculate time since last script and added new view to query all data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.prospection(models.AnalysisResult.performBIAnalysis))
   }
+  
+  def data = Action {
+    Ok(views.html.data(models.AnalysisResult.dumpData))
+  }
 
 }
--- app/models/AnalysisResult.scala
+++ app/models/AnalysisResult.scala
@@ -1,10 +1,31 @@
 package models
 import PatientType._
+import scala.collection._
 
 object AnalysisResult {
 
 	def performBIAnalysis(): Map[PatientType, Int] =
 	{
+	  // load all purchases, sort them by date and break them down by customer
+	  val purchases = PurchaseRecord
+			  			.loadFromFile
+			  			.view  					// don't explicitly construct collection 
+			  			.sortBy(_.day)
+			  			.groupBy(_.patientID)
+	  
+	  val results = mutable.Map.empty[PatientType, Int]
+	  
+	  // Initialise the return array 
+	  PatientType.values foreach {
+	    results(_) = 0
+	  }
+	  
+	  // loop over each customer
+	  purchases.values foreach {customer =>
+		  
+		  println
+	  }
+	  
 	  Map(
 	      VIOLATED -> 200,
 	      VALID_NO_COMED -> 500,
@@ -13,6 +34,19 @@ object AnalysisResult {
 	      VALID_I_TRIAL -> 50,
 	      VALID_B_TRIAL -> 50
 	  )
+	}
+	
+	def dumpData(): Map[Int, List[(PurchaseRecord, Int)]] =
+	{
+		val x = PurchaseRecord
+				.loadFromFile
+				.sortBy(_.day)
+				.groupBy(_.patientID)
+				
+		x.mapValues {t => 
+		 	t zip 0 :: (t sliding(2) map {case List(p,q) => q.day - p.day; case List(p) => 0} toList)
+			
+		}
 	}
 }
 
